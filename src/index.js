340a9b103ef768f7b50167548f1395a4d7cdaa0e
Aleksey Vaskin <alzor85@gmail.com>
2020-12-26 13:25:08 +0300

    Making project structure

--- src/index.js
+++ src/index.js
@@ -1,38 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
-// const el = React.createElement('h1',null, 'Hello world');
-
-const TodoList = () => {
-
-  const items = ['Learn React', 'Build Awesome App', 'Drink cofee'];
-  return (
-    <ul>
-      <li>{ items[0] }</li>
-      <li>{ items[1] }</li>
-      <li>{ items[2] }</li>
-    </ul>
-  );
-}
-
-const AppHeader = () => {
-  return (
-    <h1>My Todo List</h1>
-  );
-}
-
-const SearchPanel = () => {
-  const searchText = 'Type here to search';
-  const searchStyle = {
-    fontSize: '25px'
-  };
-  return (
-    <input placeholder={searchText} style={searchStyle}/>
-  );
-}
+import AppHeader from './components/app-header';
+import SearchPanel from './components/search-panel';
+import TodoList from './components/todo-list';
 
 const App = () => {
-
   return (
     <div>
       <AppHeader />
