45e1244cd4b5c14f250d3e26812e3b62a7737efe
Aleksey Vaskin <alzor85@gmail.com>
2020-12-28 11:13:48 +0300

    Adding event listener

--- src/components/todo-list-item/todo-list-item.js
+++ src/components/todo-list-item/todo-list-item.js
@@ -2,6 +2,13 @@ import React, { Component } from 'react';
 import './todo-list-item.css'
 
 export default class TodoListItem extends Component {
+
+    constructor() {
+        super();
+        this.onLabelClick = () => {
+            return console.log(`Done: ${this.props.label}`);
+        }
+    }
     
     
     render() {
@@ -15,7 +22,12 @@ export default class TodoListItem extends Component {
     
         return (
             <span className="todo-list-item">
-                <span className="todo-list-item-label" style={style}>{label}</span>
+                <span 
+                    className="todo-list-item-label" 
+                    style={style}
+                    onClick={this.onLabelClick}>
+                    {label}
+                </span>
     
                 <button type="button" className="btn btn-outline-success btn-sm float-right">
                     <i className="fa fa-exclamation" />
