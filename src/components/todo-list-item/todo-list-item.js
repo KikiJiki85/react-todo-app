b7479d815a8f2e613c3643b0ffcc75a5057898bd
Aleksey Vaskin <alzor85@gmail.com>
2020-12-28 19:46:53 +0300

    Adding state to project

--- src/components/todo-list-item/todo-list-item.js
+++ src/components/todo-list-item/todo-list-item.js
@@ -5,15 +5,29 @@ export default class TodoListItem extends Component {
 
     constructor() {
         super();
-        this.onLabelClick = () => {
-            return console.log(`Done: ${this.props.label}`);
+
+        this.state = {
+            done: false,
         }
+
+        this.onLabelClick = () => {
+           this.setState({
+            done: true
+           });
+        };
     }
     
     
     render() {
 
         const { label, important = false } = this.props;
+        const { done } = this.state;
+
+        let classNames = 'todo-list-item';
+
+        if (done) {
+            classNames += ' done';
+        }
 
         const style = {
             color: important ? 'steelblue' : 'black',
@@ -21,7 +35,7 @@ export default class TodoListItem extends Component {
           };
     
         return (
-            <span className="todo-list-item">
+            <span className={classNames}>
                 <span 
                     className="todo-list-item-label" 
                     style={style}
